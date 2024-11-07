fee89d16fec3fada8f38fc13fbab11203be0fb6a
rv-unq <ruvishushukla@gmail.com>
2024-11-07 15:08:36 +0530

    Q

---
+++ PS3.C
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int n) {
+    int start = 0, end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nOriginal Array:\n");
+    printArray(arr, n);
+
+    reverseArray(arr, n);
+
+    printf("\nReversed Array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
